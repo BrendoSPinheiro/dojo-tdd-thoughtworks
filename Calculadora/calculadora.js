be52670e65cbb5fbd3d2c10ff32862ef994aadec
Brendo Souza Pinheiro <labrendo.pinheiro@MacBook-Pro-de-Brendo.local>
2022-04-28 16:11:28 -0300

    finish

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,6 +1,22 @@
 class Calculadora {
-  static soma() {
-    //to-do
+  static soma(valor1, valor2) {
+    return valor1 + valor2;
+  }
+
+  static subtracao(valor1, valor2) {
+    return valor1 - valor2;
+  }
+
+  static multiplicacao(valor1, valor2) {
+    return valor1 * valor2;
+  }
+
+  static divisao(valor1, valor2) {
+    return valor1 / valor2;
+  }
+
+  static teste(valor1, valor2) {
+    return Math.pow(valor1, 2) + Math.pow(valor2, 2);
   }
 }
 
--- Calculadora/calculadora.test.js
+++ Calculadora/calculadora.test.js
@@ -1,7 +1,51 @@
 const Calculadora = require("./calculadora");
 
 describe("Calculadora", () => {
+  const valor1 = 2;
+  const valor2 = 5;
   test("Deve realizar a soma dos dois valores recebidos", () => {
-    // to-do
+    // setup
+    const resultadoEsperado = valor1 + valor2;
+    // implementação
+    const resultado = Calculadora.soma(valor1, valor2);
+    // asserções
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+  test("Deve realizar a subtração dos dois valores recebidos", () => {
+    // setup
+    const resultadoEsperado = valor1 - valor2;
+    // implementação
+    const resultado = Calculadora.subtracao(valor1, valor2);
+    // asserções
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+  test("Deve realizar a multiplicação dos dois valores recebidos", () => {
+    // setup
+    const resultadoEsperado = valor1 * valor2;
+    // implementação
+    const resultado = Calculadora.multiplicacao(valor1, valor2);
+    // asserções
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+  test("Deve realizar a divisao dos dois valores recebidos", () => {
+    // setup
+    const resultadoEsperado = valor1 / valor2;
+    // implementação
+    const resultado = Calculadora.divisao(valor1, valor2);
+    // asserções
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+  test("Deve realizar a soma do quadrado dos dois valores recebidos", () => {
+    // setup
+    const resultadoEsperado = Math.pow(valor1, 2) + Math.pow(valor2, 2);
+    // implementação
+    const resultado = Calculadora.teste(valor1, valor2);
+
+    // asserções
+    expect(resultado).toBe(resultadoEsperado);
   });
 });
